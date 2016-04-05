075a19c97aa645b287996a86275b20ed4cba8c19
Sandra <eloyin@ubuntu.(none)>
2016-04-05 20:51:52 +0200

    Inicial

---
+++ myproject/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'cms_put.views.newPage'),
+    url(r'(\d+)/', 'cms_put.views.pickPage'),
+    url(r'^admin/', include(admin.site.urls)),
+)
